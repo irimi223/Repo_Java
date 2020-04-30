975c16d7523c681793df1353d623d814011e2dee
Irina <irimi223@dmail.com>
2020-04-30 16:08:15 -0400

    new DataTypes

--- src/homework1/DataTypes.java
+++ src/homework1/DataTypes.java
@@ -4,14 +4,15 @@ public class DataTypes {
 
     public static void main(String[] args) {
 
-        int hrsInDay = 24;
-        int maxDaysInYear = 366;
+        byte hrsInDay = 24;
+        short maxDaysInYear = 366;
         int noOfEmployees = 5455;
-        int populationUSA = 333546000;
-        double interestRate = 2.7;
+        long populationUSA = 333546000;
+        float interestRate = 2.7f;
         double balance = 883445.98;
         boolean sunRise = false;
-        String initials = "IM";
+        char initial1 = 'I';
+        char initial2 = 'M';
         String myName = "Irina";
 
         System.out.println("1. Hours in a day = " + hrsInDay);
@@ -21,7 +22,7 @@ public class DataTypes {
         System.out.println("5. Interest Rate = " + interestRate);
         System.out.println("6. Balance in a bank account = " + balance);
         System.out.println("7. Does the sun rise from the west? " + sunRise);
-        System.out.println("8. Initials of my name: " + initials);
+        System.out.println("8. Initials of my name: " + initial1 + initial2);
         System.out.println("9. My name: " + myName);
 
     }
